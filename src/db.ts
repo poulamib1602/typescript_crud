ce09f3214420d63d47905cf7664858bc8af9eb04
poulamib1602 <poulamib.albiorix@gmail.com>
2023-10-30 17:47:29 +0530

    review changes

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,14 @@
 import mysql from 'mysql2/promise';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export async function connect() {
     const connection = await mysql.createConnection({
-        host: '127.0.0.1',
-        user: 'root',
-        password: '1234',
-        database: 'mvm',
+        host: process.env.MY_SQL_host,
+        user: process.env.MY_SQL_user,
+        password: process.env.password,
+        database: process.env.MY_SQL_database,
     });
     return connection;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { createItem, getItems, getItemById, updateItem, deleteItem } from './crud';
+import ItemService  from './crud';
 const app = express();
 
 app.use(express.json());
@@ -9,46 +9,31 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Well done!');
 });
-
-
-app.post('/', (req, res) => {
-    const requestBody = req.body;
-    res.json({ message: 'Received POST data', data: requestBody });
-});
-
-
-
-
 app.post('/items', async (req, res) => {
     const itemData = req.body;
-    const result = await createItem(itemData);
+    const result = await ItemService.createItem(itemData);
     res.json({ message: 'Item created', data: itemData, affectedRows: result });
 });
-
 app.get('/items', async (req, res) => {
-    const items = await getItems();
+    const items = await ItemService.getItems();
     res.json(items);
 });
-
 app.get('/items/:id', async (req, res) => {
     const itemId = parseInt(req.params.id);
-    const item = await getItemById(itemId);
+    const item = await ItemService.getItemById(itemId);
     res.json(item);
 });
-
 app.put('/items/:id', async (req, res) => {
     const itemId = parseInt(req.params.id);
     const itemData = req.body;
-    const result = await updateItem(itemId, itemData);
+    const result = await ItemService.updateItem(itemId, itemData);
     res.json({ message: 'Item updated', affectedRows: result });
 });
-
 app.delete('/items/:id', async (req, res) => {
     const itemId = parseInt(req.params.id);
-    const result = await deleteItem(itemId);
+    const result = await ItemService.deleteItem(itemId);
     res.json({ message: 'Item deleted', affectedRows: result });
 });
-
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
 })
